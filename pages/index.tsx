0831103931bdfebe01b56a1070374289bd94f142
Abraka Dabra <abrakadabrask@protonmail.com>
2021-07-10 13:32:10 +0200

    Added typescript support

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pokebase</title>
+        <meta name="description" content="pokebase" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Pokebase</h1>
+    </>
+  )
+}
