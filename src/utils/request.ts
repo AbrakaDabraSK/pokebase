e51c2804d02d9ed2717b30c37ddcab1c546ace06
Abraka Dabra <abrakadabrask@protonmail.com>
2021-07-10 18:42:00 +0200

    Utils|Request ver.0.0.1

---
+++ src/utils/request.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export default class Request {
+  async getHTMLContent(url: string) {
+    try {
+      const res = await axios({
+        url,
+        method: 'get',
+        headers: { 'User-Agent': process.env.USER_AGENT }
+      })
+      return res.data
+    } catch(error) {
+      throw new Error(error)
+    }
+  }
+}
