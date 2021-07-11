2fa319c6c528025ec6378d0a88e515c294600d8d
AbrakaDabraSK <83639010+AbrakaDabraSK@users.noreply.github.com>
2021-07-11 09:57:41 +0200

    Update main.tsx

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,5 @@
 const Main: React.FC = ({ children }) => (
-  <main className="flex flex-row items-stretch justify-center min-h-screen max-w-7xl mx-auto p-4 md:p-14">
+  <main className="flex flex-row items-stretch justify-center min-h-screen max-w-7xl mx-auto px-3 py-14 md:p-14">
       {children}
   </main>
 )
