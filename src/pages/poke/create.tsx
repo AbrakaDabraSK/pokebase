cd9fdc91b019c070824f37fbe2201639e16a7f86
Abraka Dabra <abrakadabrask@protonmail.com>
2021-07-11 11:49:17 +0200

    Theme ver.0.0.1 - Form Create a poke

--- src/pages/poke/create.tsx
+++ src/pages/poke/create.tsx
@@ -1,9 +1,30 @@
+import { useState, FormEvent } from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
+import Axios from 'axios'
 
 import Main from '../../components/main'
 import NavBar from '../../components/navbar/navbar'
 
 export default function Create() {
+  const [url, setUrl] = useState('')
+  const [errors, setErrors] = useState<any>({})
+
+  const router = useRouter()
+
+  const submitForm = async (e: FormEvent) => {
+    e.preventDefault()
+
+    try {
+      const res = await Axios.post('/link', {
+        url
+      })
+      router.push('/')
+    } catch (err) {
+      setErrors(err.response.data)
+    }
+  }
+
   return (
     <>
       <Head>
@@ -13,11 +34,40 @@ export default function Create() {
       </Head>
       <NavBar />
       <Main>
-        <div>
-          <h1>
-            Create a poke
-          </h1>
-        </div>
+        <form 
+          className="w-full md:w-8/12"
+          onSubmit={submitForm}
+        >
+          <fieldset className="flex flex-col items-baseline justify-center bg-white rounded shadow-sm">
+            <legend className="w-full p-6 bg-gradient-to-br from-green-700 to-green-400">
+              <h6 className="text-lg font-semibold text-white">
+                Create a poke
+              </h6>
+            </legend>
+            <div className="flex flex-col items-start justify-center w-full p-5">
+              <input
+                className="w-full mb-1 border-b border-gray-200 outline-none"
+                type="text"
+                placeholder="http://localhost"
+                value={url}
+                onChange={e => setUrl(e.target.value)}
+              />
+              <small className="mt-1 mb-3 text-sm text-gray-500 font-extralight">
+                Enter URL
+              </small>
+              <button type="submit" className="w-24 py-2 text-xs font-semibold text-white uppercase bg-green-500 border border-green-500 rounded">
+                Continue
+              </button>
+            </div>
+            {errors.message && (
+              <div className="w-full p-5 bg-red-500 rounded shadow-sm">
+                <p className="text-sm font-semibold text-white">
+                  { errors.message }
+                </p>
+              </div>
+            )}
+          </fieldset>
+        </form>
       </Main>
     </>
   )
