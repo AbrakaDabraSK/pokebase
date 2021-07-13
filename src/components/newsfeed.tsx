8bde276d134395ef0b1d8e844bf18d52c84842c1
Abraka Dabra <abrakadabrask@protonmail.com>
2021-07-13 17:12:33 +0200

    Component|Card|NewsFeedCard ver.0.0.3

--- src/components/newsfeed.tsx
+++ src/components/newsfeed.tsx
@@ -51,7 +51,7 @@ export default function NewsFeed() {
   return (
     <div className="flex-auto w-full">
       <header>
-        <h3 className="mt-1 mb-2 text-2xl font-bold text-black">
+        <h3 className="pl-1.5 md:pl-0 mt-1 mb-2 text-2xl font-bold text-black">
           Today
         </h3>
       </header>
