fe31a5a2d1baba0f80f3298feeaab780c2398d76
Abraka Dabra <abrakadabrask@protonmail.com>
2021-07-13 17:52:48 +0200

    Utils|Helpers ver.0.0.2 - Added truncate func

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -39,3 +39,6 @@ export function endIndex(currentPage: number, perPage: number): number {
 
   return endIndex
 }
+export function truncate(str: string, length: number): string {
+  return str.length > length ? str.substring(0, length) + "..." : str;
+}
