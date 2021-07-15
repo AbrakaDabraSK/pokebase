bc1dc5746ec56e88cf73c83d603bda7adc4e0e03
Abraka Dabra <abrakadabrask@protonmail.com>
2021-07-16 01:04:19 +0200

    Component|Newsfeed ver.0.0.2

--- src/components/newsfeed/block/header.tsx
+++ src/components/newsfeed/block/header.tsx
@@ -1,7 +1,8 @@
 const Header: React.FC = () => (
   <header>
-    <h3 className="pl-1.5 md:pl-0 mt-1 mb-2 text-2xl font-bold text-black">
-      Newsfeed
+    <h3 className="pl-1.5 md:pl-0 mt-1 mb-2 text-2xl font-bold text-black flex items-center justify-start">
+      <i className="mr-2 text-4xl font-semibold text-green-300 bx bx-bookmark-heart"></i>
+      <>Newsfeed</>
     </h3>
   </header>
 )
