4069fea2b8731b181d16bebf49ea0da11ea81a01
Abraka Dabra <abrakadabrask@protonmail.com>
2021-07-15 19:29:32 +0200

    dev.0.2.5

--- src/components/navbar/block/actionBar.tsx
+++ src/components/navbar/block/actionBar.tsx
@@ -4,13 +4,20 @@ const ActionBar: React.FC = () => (
   <div className="flex items-stretch justify-start">
     <div className="relative flex items-center flex-grow-0 flex-shrink-0">
       <Link href="https://github.com/AbrakaDabraSK/pokebase">
-        <a target="_blank" className="relative items-center justify-center hidden mr-2 bg-green-400 rounded-md shadow-md md:flex w-9 h-9">
-          <i className="text-white bx bxl-github"></i>
+        <a 
+          target="_blank"
+          title="GitHub Repository" 
+          className="relative items-center justify-center hidden mr-2 transition duration-300 ease-in-out delay-75 bg-green-400 rounded-md shadow-md hover:bg-green-500 md:flex w-9 h-9"
+        >
+          <i className="text-xl font-semibold text-white bx bxl-github"></i>
         </a>
       </Link>
       <Link href="/poke/create">
-        <a className="relative flex items-center justify-center bg-green-400 rounded-md shadow-md w-9 h-9">
-          <i className="text-white bx bxs-book-add"></i>
+        <a 
+          title="Create a poke"
+          className="relative flex items-center justify-center transition duration-300 ease-in-out delay-75 bg-green-400 rounded-md shadow-md w-9 h-9 hover:bg-green-500"
+        >
+          <i className="text-xl font-semibold text-white bx bxs-book-add"></i>
         </a>
       </Link>
     </div>
--- src/components/navbar/block/brand.tsx
+++ src/components/navbar/block/brand.tsx
@@ -1,16 +1,14 @@
 import Link from 'next/link'
 
 const Brand: React.FC = () => (
-  <div className="flex items-center">
-    <Link href="/">
-      <a>
-        <i className="relative mr-1 inset-0.5 text-xl bx bxs-face-mask"></i>
-        <span className="hidden text-xl font-semibold text-black lg:inline">
-          pokebase
-        </span>
-      </a>
-    </Link>
-  </div>
+  <Link href="/">
+    <a className="flex items-center justify-center">
+      <i className="mr-1 text-4xl sm:text-3xl bx bxs-face-mask"></i>
+      <span className="hidden text-xl font-semibold text-black lg:inline-block sm:mb-1">
+        pokebase
+      </span>
+    </a>
+  </Link>
 )
 
 export default Brand
