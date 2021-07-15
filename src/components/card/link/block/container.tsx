a26c565bd2f31c39cde1c82c811f282df0ef3ad8
Abraka Dabra <abrakadabrask@protonmail.com>
2021-07-15 20:38:08 +0200

    Component|Card|Link ver.0.0.4

--- src/components/card/link/block/container.tsx
+++ src/components/card/link/block/container.tsx
@@ -1,5 +1,5 @@
 const Container: React.FC = ({ children }) => (
-  <article className="relative block mb-4 overflow-hidden break-words bg-white rounded-lg shadow-lg cursor-pointer min-w-340">
+  <article className="relative block mb-4 overflow-hidden break-words bg-white border-white shadow-md cursor-pointer rounded-xl min-w-340 border-lg">
     {children}
   </article>
 )
