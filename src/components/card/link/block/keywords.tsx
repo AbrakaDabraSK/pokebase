7234fa51975550b76ff1e211d706aa2500f10975
AbrakaDabraSK <83639010+AbrakaDabraSK@users.noreply.github.com>
2021-07-16 02:04:20 +0200

    Update keywords.tsx

--- src/components/card/link/block/keywords.tsx
+++ src/components/card/link/block/keywords.tsx
@@ -6,7 +6,7 @@ const Keywords: React.FC<any> = ({ value }) => {
           {value.split(',').map((keyword, index) => (
             <span 
               key={index}
-              className="px-2 py-1 mb-1 mr-1 text-sm font-medium text-white bg-green-400 border-sm rounded-xl" 
+              className="px-2 py-1 mb-1 mr-1 text-sm font-semibold text-white bg-green-400 border-sm rounded-xl" 
             >
               { keyword }
             </span>
